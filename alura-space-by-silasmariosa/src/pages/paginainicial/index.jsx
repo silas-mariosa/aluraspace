161ed362f55710750ce367956d82588bbc62578f
silas-mariosa <silas.mariosa@hotmail.com>
2023-05-16 19:51:44 -0300

    #increment list

--- alura-space-by-silasmariosa/src/pages/paginainicial/index.jsx
+++ alura-space-by-silasmariosa/src/pages/paginainicial/index.jsx
@@ -1,7 +1,11 @@
 import Cabecalho from "../../componentes/cabecalho";
+import Menu from "../../componentes/menu";
 
 export default function PaginaInicial(){
     return(
+        <>
         <Cabecalho/>
+        <Menu/>
+        </>
     )
 }
